ff56de328ee1ff9ed1f1095a261dc94ad54ab6c3
Stephen Tsim <41970987+tsimmerdown@users.noreply.github.com>
2021-05-03 15:40:37 -0400

    added content to lander page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,113 @@
 import React from "react"
+import styled from "styled-components"
+import { Button } from "@material-ui/core"
+import MainLayout from "../templates/MainLayout"
+import Container from "../components/Cont/Container"
+import lander from "../assets/images/Lander.jpg"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import ProductCard from "../components/ProductCard/ProductCard"
+import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+
+const LanderCont = styled(Container)`
+  display: flex;
+  height: 800px;
+  justify-content: center;
+  flex-direction: column;
+  padding-top: 80px;
+  background-color: #f2f2f0;
+  background-image: url(${lander});
+  object-fit: contain;
+  background-size: 65vw 900px;
+  background-repeat: no-repeat;
+  background-position: right center;
+`
+
+const Goodies = styled(Container)`
+  min-height: 800px;
+`
+
+const ProductCont = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 48px 0 96px 0;
+`
+
+const Typography = styled.p`
+  margin-bottom: ${props => (props.small ? "32px" : "40px")};
+  font-size: ${props => (props.small ? "20px" : "80px")};
+  font-weight: ${props => (props.small ? "400" : "600")};
+`
+
+const ShopButton = styled(Button)`
+  height: 64px;
+  width: 240px;
+  && {
+    z-index: 0;
+    border-radius: 50px;
+    background-color: #f17c98;
+    color: white;
+    font-size: 24px;
+    &:hover {
+      color: black;
+    }
+  }
+`
+
+export default function Home({ data }) {
+  return (
+    <MainLayout>
+      <LanderCont>
+        <Typography small>freshly made for sharing</Typography>
+        <Typography
+          style={{
+            maxWidth: "850px",
+            lineHeight: "100px",
+          }}
+        >
+          A bundle of joy <br /> for every occasion
+        </Typography>
+        <ShopButton>Shop Now</ShopButton>
+      </LanderCont>
+      <Goodies>
+        <StaticImage src="../" />
+        <Typography style={{ margin: "10vh 0 15vh" }}>
+          Popular Goodies
+        </Typography>
+        <ProductCont>
+          {data.allContentfulFeaturedProductCard.nodes.map(card => {
+            return (
+              <ProductCard
+                name={card.productName}
+                startingPrice={card.price}
+                key={card.id}
+                slug={card.slug}
+                img={card.image}
+              />
+            )
+          })}
+        </ProductCont>
+      </Goodies>
+    </MainLayout>
+  )
 }
+
+export const query = graphql`
+  {
+    allContentfulFeaturedProductCard {
+      nodes {
+        slug
+        productName
+        price
+        id
+        image {
+          gatsbyImageData(
+            placeholder: BLURRED
+            formats: [AUTO, WEBP]
+            width: 300
+          )
+        }
+      }
+    }
+  }
+`
